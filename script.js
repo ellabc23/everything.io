4d1625d083918736d43c64851e659e1ebf7aaf7e
Ella <33379153+ellabc23@users.noreply.github.com>
2019-11-23 10:11:17 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+var username = document.getElementById('username').value;
+var password =  document.getElementById('password').value;
+if ((username=='ellabc23'&& password=='')||(username=='ellabc23'&& password=='')){
+window.location.href='home.html';
+}
+} 
