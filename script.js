e28b3da640627533685c9c3a33c3e42a562b3357
Ella <33379153+ellabc23@users.noreply.github.com>
2019-11-23 10:54:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
-if ((username=='ellabc23'&& password=='')||(username=='zahara'&& password=='zahara')||(username=='flowerjasmine'&& password=='YASSister')||(username=='isabelle'&& password=='isabelle')){
+if ((username=='ellabc23'&& password=='everything!!!')||(username=='zahara'&& password=='zahara')||(username=='flowerjasmine'&& password=='YASSister')||(username=='isabelle'&& password=='isabelle')){
 window.location.href='home.html';
 }
 } 
